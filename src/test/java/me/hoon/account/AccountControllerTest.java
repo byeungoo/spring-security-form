67d0699a17c6bdbd64d0e7f4914f8f83a980b53f
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-09 16:28:03 +0900

    스프링 시큐리티 Controller 테스트 코드 작성

---
+++ src/test/java/me/hoon/account/AccountControllerTest.java
@@ -0,0 +1,91 @@
+package me.hoon.account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void index_anonymous() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void index_anonymous2() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void index_user() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/").with(user("goohoon").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "goohoon", roles = "USER")
+    public void index_user2() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUser
+    public void index_user3() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void admin() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin").with(user("admin").roles("ADMIN")))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void admin2() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void not_admin() throws Exception{
+        //이러한 유저가 로그인한 상태일 경우라고 가정
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin").with(user("goohoon").roles("USER")))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+}
---
+++ src/test/java/me/hoon/account/WithUser.java
@@ -0,0 +1,11 @@
+package me.hoon.account;
+
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithMockUser(username = "goohoon", roles="USER")
+public @interface WithUser {
+}
