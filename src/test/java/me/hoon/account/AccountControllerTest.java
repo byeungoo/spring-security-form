4c977cb756770a7583e50b3cbe6f2f5024221e83
ByeonGooHoon <dksekfldks65@naver.com>
2020-10-09 16:58:03 +0900

    스프링 시큐리티 form login 인증 테스트 코드 작성

--- src/test/java/me/hoon/account/AccountControllerTest.java
+++ src/test/java/me/hoon/account/AccountControllerTest.java
@@ -7,10 +7,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +25,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     public void index_anonymous() throws Exception{
@@ -88,4 +94,33 @@ public class AccountControllerTest {
                 .andDo(print())
                 .andExpect(status().isForbidden());
     }
+
+    @Test
+    @Transactional
+    public void login_success() throws Exception{
+        String username = "goohoon";
+        String password = "123";
+        Account user = this.createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password(password))
+                .andExpect(SecurityMockMvcResultMatchers.authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_fail() throws Exception{
+        String username = "goohoon";
+        String password = "123";
+        Account user = this.createUser(username, password);
+        mockMvc.perform(formLogin().user(username).password("12345"))
+                .andExpect(SecurityMockMvcResultMatchers.unauthenticated());
+    }
+
+    public Account createUser(String username, String password){
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+        return accountService.createNew(account);
+    }
+
 }
